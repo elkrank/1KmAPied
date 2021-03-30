b6afb63452835fb3341c8dab36e153e65173b52d
rémy Letailleur <39468140+elkrank@users.noreply.github.com>
2021-03-30 09:44:40 +0200

    Update map2km-d.js

--- js/2km/map2km-d.js
+++ js/2km/map2km-d.js
@@ -26,7 +26,7 @@ var lngLat = {
 };
 
 var tabCoordonnees = [];
-var defaultKey = "9abef042-bba4-4ca2-8ca7-071d2e0185c7";
+//var defaultKey = "9abef042-bba4-4ca2-8ca7-071d2e0185c7";
 var userInput = document.getElementById("adresse").value;
 
 function geocode(){
