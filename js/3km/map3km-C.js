670e8eca8b398a14634dd85d1f324be7f1d04d20
Alica-B <74233598+Alica-B@users.noreply.github.com>
2021-04-08 11:39:07 +0200

    Update map3km-C.js

--- js/3km/map3km-C.js
+++ js/3km/map3km-C.js
@@ -11,6 +11,7 @@ var map = new mapboxgl.Map({
 	style: 'mapbox://styles/mapbox/streets-v11', // Specify which map style to use
 	center: [lon, lat], // Specify the starting position
 	zoom: 15, // Specify the starting zoom
+	preserveDrawingBuffer: true
 });
 
 //Marqueur de position
